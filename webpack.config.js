c9f9c8070c6ce086b47b78310133d43c09ebcd15
jpavelka <jeffp171@gmail.com>
2021-09-10 14:51:30 +0000

    upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = (env, options) => {
-  env = env || {}
-  let entry = ["@babel/polyfill", './src/app.js'];
-  if (env.dataSource == 'saved'){
-    entry[1] = './src/app-saved-data.js'
+  env = env || {};
+  let entry = ["@babel/polyfill", "./src/app.js"];
+  if (env.dataSource == "saved") {
+    entry[1] = "./src/app-saved-data.js";
   }
   let plugins = [
     new HtmlWebpackPlugin({
-        title: "CFB Games",
-    })
-  ]
-  if (options.mode == 'production'){
-    plugins.push(new CleanWebpackPlugin())
+      title: "CFB Games",
+    }),
+  ];
+  if (options.mode == "production") {
+    plugins.push(new CleanWebpackPlugin());
   }
 
   return {
     entry: entry,
     output: {
-        filename: '[name].bundle.[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+      filename: "[name].bundle.[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
     },
     module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
-          {
-            test: /\.css$/,
-            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-          },
-        ]
-      },
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        },
+      ],
+    },
     plugins: plugins,
     devServer: {
-        contentBase: './dist',
-        open: true
+      static: "./dist",
+      open: true,
+      allowedHosts: ["all"],
     },
-  }
+  };
 };
