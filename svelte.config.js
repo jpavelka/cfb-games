18a61f57d57b9c32800c0f322382090e8df56e6e
jpavelka <jeffp171@gmail.com>
2022-09-03 20:16:08 +0000

    set up static site rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		prerender: {
+		  // This can be false if you're using a fallback (i.e. SPA mode)
+		  default: false
+		}
 	},
 	preprocess: preprocess()
 };
