250937f3f043dde0da1e5746dad2b5edb6a48406
jpavelka <jeffp171@gmail.com>
2022-09-07 20:29:02 +0000

    workflow

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	server: {
-		fs: {
-		  allow: ['..']
-		}
-	}
+	// server: {
+	// 	fs: {
+	// 	  allow: ['..']
+	// 	}
+	// }
 };
 
 export default config;
